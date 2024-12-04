6d3395041cbe1c3bcda6d8ecce568dbaa40a5f90
Relfost <relfost@ya.ru>
2024-12-04 21:50:10 +0300

    Initial deploy and overwrite repository

---
+++ README.md
@@ -0,0 +1,88 @@
+
+# System.Collections.Generic.Extension
+
+This library provides a set of extension methods for `Dictionary<TKey, TValue>` in .NET Framework 4.8.1. These extensions are designed to simplify and enhance common operations such as adding, updating, and retrieving dictionary entries.
+
+## Features
+
+- **ContainsAllKeys / ContainsKeys**  
+  Checks if all keys from a given collection exist in the dictionary.
+
+- **ContainsAnyKey**  
+  Checks if at least one key from a given collection exists in the dictionary.
+
+- **GetOrAdd**  
+  Retrieves the value for a key or adds a new value using either a factory function, a default value, or an explicitly provided value.
+
+- **UpdateOrAdd**  
+  Updates an existing entry or adds a new one. Supports direct updates, actions, or custom value factories.
+
+## Installation
+
+Copy the `System.Collections.Generic.Extension.cs` file into your project. Make sure your project targets **.NET Framework 4.8.1**.
+
+## Usage
+
+### ContainsAllKeys / ContainsKeys
+
+```csharp
+var dictionary = new Dictionary<string, int>
+{
+    { "key1", 1 },
+    { "key2", 2 }
+};
+
+var keys = new List<string> { "key1", "key2" };
+
+bool allKeysExist = dictionary.ContainsAllKeys(keys); // True
+bool allKeysExistAlternative = dictionary.ContainsKeys(keys); // True
+```
+
+### ContainsAnyKey
+
+```csharp
+var keysToCheck = new List<string> { "key2", "key3" };
+bool anyKeyExists = dictionary.ContainsAnyKey(keysToCheck); // True
+```
+
+### GetOrAdd
+
+```csharp
+var dictionary = new Dictionary<string, int>();
+
+// Using a factory function
+int value = dictionary.GetOrAdd("key1", () => 42); // Adds and returns 42
+
+// Using the default value
+int defaultValue = dictionary.GetOrAdd("key2"); // Adds and returns 0
+
+// Using an explicitly provided default value
+int customDefault = dictionary.GetOrAdd("key3", 10); // Adds and returns 10
+```
+
+### UpdateOrAdd
+
+```csharp
+var dictionary = new Dictionary<string, int>();
+
+// Direct update or add
+dictionary.UpdateOrAdd("key1", 20); // Adds "key1" with value 20
+
+// Update with an action
+dictionary.UpdateOrAdd("key1", value => value += 10); // Updates "key1" to 30
+
+// Update or add with a factory
+dictionary.UpdateOrAdd("key2", value => value += 5, () => 50); // Adds "key2" with value 50
+```
+
+## Requirements
+
+- .NET Framework 4.8.1
+
+## Contributing
+
+Feel free to fork this repository and submit pull requests. All contributions are welcome!
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/System/Collections/Generic.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        public static bool ContainsKeys<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, IEnumerable<TKey> keys)
+        {
+            return dictionary.ContainsAllKeys(keys);
+        }
+
+        public static bool ContainsAllKeys<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, IEnumerable<TKey> keys)
+        {
+            foreach (var key in keys)
+            {
+                if (!dictionary.ContainsKey(key))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool ContainsAnyKey<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, IEnumerable<TKey> keys)
+        {
+            foreach (var key in keys)
+            {
+                if (dictionary.ContainsKey(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<TValue> valueFactory)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                var value = valueFactory();
+                dictionary.Add(key, value);
+                return value;
+            }
+            return dictionary[key];
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return dictionary.GetOrAdd(key, () => default);
+        }
+
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            return dictionary.GetOrAdd(key, () => defaultValue);
+        }
+
+        public static void UpdateOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Action<TValue> updateAction, Func<TValue> createFactory)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                updateAction(dictionary[key]);
+            }
+            else
+            {
+                var newValue = createFactory();
+                dictionary.Add(key, newValue);
+            }
+        }
+
+        public static void UpdateOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+        }
+
+        public static void UpdateOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Action<TValue> updateAction) where TValue : new()
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                updateAction(dictionary[key]);
+            }
+            else
+            {
+                var newValue = new TValue();
+                dictionary.Add(key, newValue);
+                updateAction(newValue);
+            }
+        }
+    }
+}
